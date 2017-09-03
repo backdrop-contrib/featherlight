34f51adc528d3e92af2de9e8af39d588051399fb
Peter Anderson <peter@panda.id.au>
2017-09-04 08:11:35 +1000

    Fixed captions.

--- js/featherlight.caption.js
+++ js/featherlight.caption.js
@@ -9,7 +9,10 @@ Backdrop.behaviors.featherlightCaption = {
   attach: function(context, settings) {
 
     $.featherlight.prototype.afterContent = function() {
-      var caption = this.$currentTarget.find('img').attr(settings.featherlight.caption);
+      var imgData = this.$currentTarget.find('img');
+      var fileID = imgData.attr('data-file-id');
+      var captionAttr = settings.featherlight['id-' + fileID]['captionAttr'];
+      var caption = imgData.attr(captionAttr);
       this.$instance.find('.featherlight-caption').remove();
       $('<div class="featherlight-caption">').text(caption).appendTo(this.$instance.find('.featherlight-content'));
     };
